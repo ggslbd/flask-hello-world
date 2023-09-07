1d1068a425a2a83891a4ec3e2bb7ddcf953b542c
mdsabur <69805938+ggslbd@users.noreply.github.com>
2023-09-07 15:05:48 +0600

    add changes with templates
    Some changes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')
+def index():
+    name = 'Tour Bangaldesh'
+    return render_template('index.html', title='Welcome', username=name)
+
+
+app.run(host='0.0.0.0', port=81)
